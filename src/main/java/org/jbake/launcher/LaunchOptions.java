4022d7f1e8bf796b5917abf3703f32165361d931
Jonathan Bullock <jonbullock@gmail.com>
2013-04-26 18:19:36 +0100

    Updated usage text and changed default destination directory.

--- src/main/java/org/jbake/launcher/LaunchOptions.java
+++ src/main/java/org/jbake/launcher/LaunchOptions.java
@@ -6,10 +6,10 @@ import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 class LaunchOptions {
-	@Argument(index = 0, usage = "source folder of site content (with templates and assets) defaults to current working directory", metaVar = "source_folder")
+	@Argument(index = 0, usage = "source folder of site content (with templates and assets), if not supplied will default to current working directory", metaVar = "source_folder")
 	private File source = new File(".");
 
-	@Argument(index = 1, usage = "destination folder for baked output, defaults to a folder called \"baked\" in the current working directory", metaVar = "destination_folder")
+	@Argument(index = 1, usage = "destination folder for output, if not supplied will default to a folder called \"output\" in the current working directory, can be overridden by setting destination.folder in custom.properties", metaVar = "destination_folder")
 	private File destination = null;
 
 	@Option(name = "-h", aliases = {"--help"}, usage="prints this message")
