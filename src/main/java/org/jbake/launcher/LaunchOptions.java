4451dd3f2bbd8879d1808a7d29fac9d3af9a1bf6
Jonathan Bullock <jonbullock@gmail.com>
2013-04-26 13:49:17 +0100

    Modified usage text.

--- src/main/java/org/jbake/launcher/LaunchOptions.java
+++ src/main/java/org/jbake/launcher/LaunchOptions.java
@@ -6,17 +6,14 @@ import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 class LaunchOptions {
-	@Argument(index = 0, usage = "source of your blog posts (with templates and assets)", metaVar = "source_folder")
+	@Argument(index = 0, usage = "source folder of site content (with templates and assets) defaults to current working directory", metaVar = "source_folder")
 	private File source = new File(".");
 
-	@Argument(index = 1, usage = "destination folder for baked artifacts", metaVar = "destination_folder")
+	@Argument(index = 1, usage = "destination folder for baked output, defaults to a folder called \"baked\" in the current working directory", metaVar = "destination_folder")
 	private File destination = null;
 
 	@Option(name = "-h", aliases = {"--help"}, usage="prints this message")
 	private boolean isHelpNeeded;
-
-	
-	LaunchOptions() {}
 
 	File getSource() {
 		return source;
