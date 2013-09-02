2938ec9c82680a5ca3b93f65082e8354754a204d
Jonathan Bullock <jonbullock@gmail.com>
2013-09-02 13:57:37 +0100

    More work on #15 now supports JBake metadata to be embedded inside AsciiDoc content.

--- src/main/java/org/jbake/app/Parser.java
+++ src/main/java/org/jbake/app/Parser.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import org.apache.commons.io.IOUtils;
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.Asciidoctor.Factory;
+import org.asciidoctor.DocumentHeader;
 
 import com.petebevin.markdown.MarkdownProcessor;
 
@@ -53,6 +54,14 @@ public class Parser {
 			if (validFile) {
 				processHeader(fileContents);
 				processBody(fileContents, file);
+			} else if (file.getPath().endsWith(".asciidoc") || file.getPath().endsWith(".ad")) {
+				validFile = validateAsciiDocHeader(file);
+				if (validFile) {
+					processAsciiDocHeader(file);
+					processBody(fileContents, file);
+				} else {
+					return null;
+				}
 			} else {
 				return null;
 			}			
@@ -136,6 +145,85 @@ public class Parser {
 	}
 	
 	/**
+	 * Validate the header of an AsciiDoc file.
+	 * 
+	 * @param contents	Contents of file 
+	 */
+	private boolean validateAsciiDocHeader(File file) {
+		Asciidoctor asciidoctor = Factory.create();
+		DocumentHeader header = asciidoctor.readDocumentHeader(file);
+		boolean statusFound = false;
+		boolean typeFound = false;
+		
+		for (String key : header.getAttributes().keySet()) {
+			if (key.equals("jbake-status")) {
+				statusFound = true;
+			}
+			if (key.equals("jbake-type")) {
+				typeFound = true;
+			}
+		}
+		
+		if (!statusFound || !typeFound) {
+			System.out.println("");
+			if (!statusFound) {
+				System.out.println("Missing required header tag: jbake-status");
+			}
+			if (!typeFound) {
+				System.out.println("Missing required header tag: jbake-type");
+			}
+			
+			return false;
+		}
+		return true;		
+	}
+	
+	/**
+	 * Process the header of an AsciiDoc file.
+	 * 
+	 * @param contents	Contents of file 
+	 */
+	private void processAsciiDocHeader(File file) {
+		Asciidoctor asciidoctor = Factory.create();
+		DocumentHeader header = asciidoctor.readDocumentHeader(file);
+//		header.getAttributes().get("docdate")
+//		header.getAttributes().get("awestruct-tags")
+		if (header.getDocumentTitle() != null) {
+			content.put("title", header.getDocumentTitle());
+		}
+		Map<String, Object> attributes = header.getAttributes(); 
+		for (String key : attributes.keySet()) {
+			if (key.equals("jbake-status")) {
+				if (attributes.get(key) != null) {
+					content.put("status", attributes.get(key));
+				}
+			} else if (key.equals("jbake-type")) {
+				if (attributes.get(key) != null) {
+					content.put("type", attributes.get(key));
+				}
+			} else if (key.equals("docdate")) {
+				if (attributes.get(key) != null && attributes.get(key) instanceof String) {
+					
+					DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+					Date date = null;
+					try {
+						date = df.parse((String)attributes.get(key));
+						content.put("date", date);
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
+			} else if (key.equals("jbake-tags")) {
+				if (attributes.get(key) != null && attributes.get(key) instanceof String) {
+					content.put("tags", ((String)attributes.get(key)).split(","));
+				}
+			} else {
+				content.put(key, attributes.get(key));
+			}
+		}
+	}
+	
+	/**
 	 * Process the body of the file.
 	 * 
 	 * @param contents	Contents of file
@@ -150,6 +238,12 @@ public class Parser {
 			}
 			if (line.equals("~~~~~~")) {
 				inBody = true;
+			}
+		}
+		
+		if (body.length() == 0) {
+			for (String line : contents) {
+				body.append(line + "\n");
 			}
 		}
 		
