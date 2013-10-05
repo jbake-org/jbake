1f28b91dc520c340a41f12ded9ac62f01ba2f114
Jonathan Bullock <jonbullock@gmail.com>
2013-10-05 23:12:23 +0100

    Removed debug code.

--- src/test/java/org/jbake/app/ConfigUtilTest.java
+++ src/test/java/org/jbake/app/ConfigUtilTest.java
@@ -18,21 +18,9 @@ public class ConfigUtilTest {
 	public void load() throws Exception {
 		ConfigUtil.reset();
 		CompositeConfiguration config = ConfigUtil.load(new File(this.getClass().getResource("/").getFile()));
+		
 		// check default.properties values exist
 		Assert.assertEquals("output", config.getString("destination.folder"));	
-		
-//		File path = new File(this.getClass().getResource("/").getFile());
-//		System.out.println(path.getPath());
-//		for (File file : path.listFiles()) {
-//			if (file.getName().equalsIgnoreCase("custom.properties")) {
-//				BufferedReader reader = new BufferedReader(new FileReader(file));
-//				List<String> fileContents = IOUtils.readLines(reader);
-//				for (String line : fileContents) {
-//					System.out.println(line);
-//				}
-//			}
-//			System.out.println(file.getPath());
-//		}
 		
 		// check custom.properties values exist
 		Assert.assertEquals("testing123", config.getString("test.property"));
