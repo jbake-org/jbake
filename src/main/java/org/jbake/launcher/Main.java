8930eee89dac3209a8620c5db75abfd468acd083
RainerW <github@inforw.de>
2014-01-05 00:44:09 +0100

    Added "bake" param to support bake + serving in one step

--- src/main/java/org/jbake/launcher/Main.java
+++ src/main/java/org/jbake/launcher/Main.java
@@ -1,36 +1,15 @@
 package org.jbake.launcher;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
 
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.ConfigurationUtils;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.server.nio.NetworkTrafficSelectChannelConnector;
 import org.jbake.app.ConfigUtil;
 import org.jbake.app.FileUtil;
 import org.jbake.app.Oven;
-import org.jbake.app.ZipUtil;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
 /**
  * Launcher for JBake.
@@ -39,7 +18,6 @@ import org.jbake.app.ZipUtil;
  *
  */
 public class Main {
-//	public static final String VERSION = "v2.2";
 
 	private final String USAGE_PREFIX = "Usage: jbake";
 	
@@ -49,11 +27,10 @@ public class Main {
 	 * @param String[] args
 	 */
 	public static void main(String[] args) {
-		Main m = new Main();
-		m.run(m.parseArguments(args));
+		new Main().run(args);
 	}
 	
-	private void run(LaunchOptions options) {
+	private void bake(LaunchOptions options) {
 		try {
 			Oven oven = new Oven(options.getSource(), options.getDestination());
 			oven.setupPaths();
@@ -64,48 +41,65 @@ public class Main {
 		}
 	}
 
+	private void run(String[] args) {
+		LaunchOptions res = parseArguments(args);
+
+		CompositeConfiguration config = null;
+		try {
+			config = ConfigUtil.load(res.getSource());
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		System.out.println("JBake " + config.getString("version") + " (" + config.getString("build.timestamp") + ") [http://jbake.org]");
+		System.out.println();
+		boolean didSomething=false;
+		
+		if (res.isHelpNeeded()) {
+			didSomething=true;
+			printUsage(res);
+		}
+		
+		if(res.isBake()) {
+			didSomething=true;
+			bake(res);
+		}
+
+		if (res.isRunServer()) {
+			didSomething=true;
+			if (res.getSource().getPath().equals(".")) {
+				// use the default destination folder
+				runServer(config.getString("destination.folder"), config.getString("server.port"));
+			} else {
+				runServer(res.getSource().getPath(), config.getString("server.port"));
+			}
+		}
+		
+		if (res.isInit()) {
+			didSomething=true;
+			initStructure(config);
+		}
+		
+		if(!didSomething) {
+			bake(res);
+		}
+	}
 	private LaunchOptions parseArguments(String[] args) {
 		LaunchOptions res = new LaunchOptions();
 		CmdLineParser parser = new CmdLineParser(res);
 
 		try {
 			parser.parseArgument(args);
-
-			CompositeConfiguration config = null;
-			try {
-				config = ConfigUtil.load(res.getSource());
-			} catch (ConfigurationException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-			
-			System.out.println("JBake " + config.getString("version") + " (" + config.getString("build.timestamp") + ") [http://jbake.org]");
-			System.out.println();
-			
-			if (res.isHelpNeeded()) {
-				printUsage(parser);
-			}
-			
-			if (res.isRunServer()) {
-				if (res.getSource().getPath().equals(".")) {
-					// use the default destination folder
-					runServer(config.getString("destination.folder"), config.getString("server.port"));
-				} else {
-					runServer(res.getSource().getPath(), config.getString("server.port"));
-				}
-			}
-			
-			if (res.isInit()) {
-				initStructure(config);
-			}
 		} catch (CmdLineException e) {
-			printUsage(parser);
+			printUsage(res);
 		}
 
 		return res;
 	}
 
-	private void printUsage(CmdLineParser parser) {
+	private void printUsage(Object options) {
+		CmdLineParser parser = new CmdLineParser(options);
 		StringWriter sw = new StringWriter();
 		sw.append(USAGE_PREFIX);
 		parser.printSingleLineUsage(sw, null);
@@ -117,7 +111,7 @@ public class Main {
 
 	private void runServer(String path, String port) {
 		JettyServer.run(path, port);
-        System.exit(0);
+		System.exit(0);
 	}
 	
 	private void initStructure(CompositeConfiguration config) {
