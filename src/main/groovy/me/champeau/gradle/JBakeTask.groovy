9a209728112c82f45508582dd7a072fe1e4bc82c
Cedric Champeau <cedric.champeau@gmail.com>
2014-01-29 12:18:58 +0100

    Initial commit

---
+++ src/main/groovy/me/champeau/gradle/JBakeTask.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.champeau.gradle
+
+import org.apache.commons.configuration.CompositeConfiguration
+import org.apache.commons.configuration.MapConfiguration
+import org.gradle.api.internal.AbstractTask
+import org.gradle.api.tasks.TaskAction
+import org.jbake.app.Oven
+
+class JBakeTask extends AbstractTask {
+    @TaskAction
+    void bake() {
+        new Oven(project.jbake.input, project.jbake.output, project.jbake.clearCache).with {
+            config = new CompositeConfiguration([new MapConfiguration(project.jbake.configuration), config])
+            setupPaths()
+            bake()
+        }
+    }
+
+}
