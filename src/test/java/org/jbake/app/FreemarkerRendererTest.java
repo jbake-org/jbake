0876c1e51c298d33ba048f894d86ec62ca1d6a52
Jason Lee <jason@steeplesoft.com>
2015-02-11 09:06:37 -0600

    Create test for Freemarker rendering Add assets for FM test Remove unused import in ThymeleafRenderTest

--- src/test/java/org/jbake/app/FreemarkerRendererTest.java
+++ src/test/java/org/jbake/app/FreemarkerRendererTest.java
@@ -1,10 +1,37 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 jdlee.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jbake.app;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.io.FileUtils;
-import org.jbake.app.ConfigUtil.Keys;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.jbake.model.DocumentTypes;
 import org.junit.After;
 import org.junit.Assert;
@@ -13,14 +40,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Iterator;
-import java.util.Map;
-
-public class ThymeleafRendererTest {
+/**
+ *
+ * @author jdlee
+ */
+public class FreemarkerRendererTest {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -42,7 +66,7 @@ public class ThymeleafRendererTest {
 
         destinationFolder = folder.getRoot();
 
-        templateFolder = new File(sourceFolder, "thymeleafTemplates");
+        templateFolder = new File(sourceFolder, "freemarkerTemplates");
         if (!templateFolder.exists()) {
             throw new Exception("Cannot find template folder!");
         }
@@ -53,12 +77,11 @@ public class ThymeleafRendererTest {
             String key = keys.next();
             if (key.startsWith("template") && key.endsWith(".file")) {
                 String old = (String)config.getProperty(key);
-                config.setProperty(key, old.substring(0, old.length()-4)+".thyme");
+                config.setProperty(key, old.substring(0, old.length()-4)+".ftl");
             }
         }
-        Assert.assertEquals(".html", config.getString(Keys.OUTPUT_EXTENSION));
+        Assert.assertEquals(".html", config.getString(ConfigUtil.Keys.OUTPUT_EXTENSION));
         db = DBUtil.createDB("memory", "documents"+System.currentTimeMillis());
-        
     }
 
     @After
@@ -70,7 +93,7 @@ public class ThymeleafRendererTest {
     @Test
     public void renderPost() throws Exception {
     	// setup
-    	Crawler crawler = new Crawler(db, sourceFolder, config);
+        Crawler crawler = new Crawler(db, sourceFolder, config);
         crawler.crawl(new File(sourceFolder.getPath() + File.separator + "content"));
         Parser parser = new Parser(config, sourceFolder.getPath());
         Renderer renderer = new Renderer(db, destinationFolder, templateFolder, config);
@@ -86,18 +109,15 @@ public class ThymeleafRendererTest {
         // verify
         String output = FileUtils.readFileToString(outputFile);
         assertThat(output) 
-        	.contains("<h2>Second Post</h2>")
-        	.contains("<p class=\"post-date\">28")
-        	.contains("2013</p>")
-        	.contains("Lorem ipsum dolor sit amet")
-        	.contains("<h5>Published Posts</h5>")
-        	.contains("blog/2012/first-post.html");
+        	.contains("<h1>Second Post</h1>")
+        	.contains("<p><em>28 February 2013</em></p>")
+        	.contains("Lorem ipsum dolor sit amet");
     }
     
     @Test
     public void renderPage() throws Exception {
     	// setup
-    	Crawler crawler = new Crawler(db, sourceFolder, config);
+        Crawler crawler = new Crawler(db, sourceFolder, config);
         crawler.crawl(new File(sourceFolder.getPath() + File.separator + "content"));
         Parser parser = new Parser(config, sourceFolder.getPath());
         Renderer renderer = new Renderer(db, destinationFolder, templateFolder, config);
@@ -113,10 +133,8 @@ public class ThymeleafRendererTest {
         // verify
         String output = FileUtils.readFileToString(outputFile);
         assertThat(output) 
-	        .contains("<h4>About</h4>")
-	    	.contains("All about stuff!")
-	    	.contains("<h5>Published Pages</h5>")
-	    	.contains("/projects.html");
+	        .contains("<title>About</title>")
+	    	.contains("All about stuff!");
     }
 
     @Test
@@ -135,8 +153,8 @@ public class ThymeleafRendererTest {
         // verify
         String output = FileUtils.readFileToString(outputFile);
         assertThat(output) 
-        	.contains("<h4><a href=\"blog/2012/first-post.html\" shape=\"rect\">First Post</a></h4>")
-        	.contains("<h4><a href=\"blog/2013/second-post.html\" shape=\"rect\">Second Post</a></h4>");
+        	.contains("<a href=\"blog/2012/first-post.html\">")
+        	.contains("<a href=\"blog/2013/second-post.html\">");
     }
 
     @Test
@@ -168,8 +186,8 @@ public class ThymeleafRendererTest {
         // verify
         String output = FileUtils.readFileToString(outputFile);
         assertThat(output) 
-        	.contains("<a href=\"blog/2013/second-post.html\" shape=\"rect\">Second Post</a></h4>")
-        	.contains("<a href=\"blog/2012/first-post.html\" shape=\"rect\">First Post</a></h4>");
+        	.contains("<a href=\"blog/2013/second-post.html\">")
+        	.contains("<a href=\"blog/2012/first-post.html\">");
     }
 
     @Test
@@ -184,13 +202,12 @@ public class ThymeleafRendererTest {
         Assert.assertTrue(outputFile.exists());
         String output = FileUtils.readFileToString(outputFile);
         assertThat(output) 
-        	.contains("<a href=\"blog/2013/second-post.html\" shape=\"rect\">Second Post</a></h4>")
-        	.contains("<a href=\"blog/2012/first-post.html\" shape=\"rect\">First Post</a></h4>");
+        	.contains("<a href=\"blog/2013/second-post.html\">")
+        	.contains("<a href=\"blog/2012/first-post.html\">");
     }
 
     @Test
     public void renderSitemap() throws Exception {
-    	// cannot be put in @Before as ThmyeLeaf can't lazy load from DB, and expects content there under this type
     	DocumentTypes.addDocumentType("paper");
     	DBUtil.updateSchema(db);
     	
@@ -209,4 +226,5 @@ public class ThymeleafRendererTest {
         	.contains("papers/published-paper.html")
         	.doesNotContain("draft-paper.html");
     }
+    
 }
