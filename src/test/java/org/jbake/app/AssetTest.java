7db703671ed9f5dcb2622db946cff47c88798746
Peter Palmreuther <pitpalme+github@gmail.com>
2015-05-08 08:38:33 +0200

    Enhance AssetTest to raise code coverage during tests

--- src/test/java/org/jbake/app/AssetTest.java
+++ src/test/java/org/jbake/app/AssetTest.java
@@ -21,10 +21,14 @@ public class AssetTest {
     public void setup() throws Exception, IOException, URISyntaxException {
 		config = ConfigUtil.load(new File(this.getClass().getResource("/").getFile()));
         Assert.assertEquals(".html", config.getString(Keys.OUTPUT_EXTENSION));
+		readOnlyFolder.getRoot().setReadOnly();
 	}
 
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
+
+	@Rule
+	public TemporaryFolder readOnlyFolder = new TemporaryFolder();
 
 	@Test
 	public void copy() throws Exception {
@@ -39,6 +43,8 @@ public class AssetTest {
 		Assert.assertTrue("File " + imgFile.getAbsolutePath() + " does not exist", imgFile.exists());
 		File jsFile = new File(folder.getRoot().getPath() + File.separatorChar + "js" + File.separatorChar + "bootstrap.min.js");
 		Assert.assertTrue("File " + jsFile.getAbsolutePath() + " does not exist", jsFile.exists());
+
+		Assert.assertTrue("Errors during asset copying", asset.getErrors().isEmpty());
 	}
 
 	@Test
@@ -51,6 +57,8 @@ public class AssetTest {
 
 		File favFile = new File(folder.getRoot().getPath() + File.separatorChar + "favicon.ico");
 		Assert.assertTrue("File " + favFile.getAbsolutePath() + " does not exist", favFile.exists());
+
+		Assert.assertTrue("Errors during asset copying", asset.getErrors().isEmpty());
 	}
 
 	@Test
@@ -66,5 +74,36 @@ public class AssetTest {
 		Assert.assertTrue("File " + testFile.getAbsolutePath() + " does not exist", testFile.exists());
 		File testIgnoreFile = new File(folder.getRoot(), ".test.txt");
 		Assert.assertFalse("File " + testIgnoreFile.getAbsolutePath() + " does exist", testIgnoreFile.exists());
+
+		Assert.assertTrue("Errors during asset copying", asset.getErrors().isEmpty());
+	}
+
+	/**
+	 * Primary intention is to extend test cases to increase coverage.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testWriteProtected() throws Exception {
+		URL assetsUrl = this.getClass().getResource("/assets");
+		File assets = new File(assetsUrl.getFile());
+		Asset asset = new Asset(assets.getParentFile(), readOnlyFolder.getRoot(), config);
+		asset.copy(assets);
+
+		Assert.assertFalse("At least one error during copy expected", asset.getErrors().isEmpty());
+	}
+
+	/**
+	 * Primary intention is to extend test cases to increase coverage.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testUnlistable() throws Exception {
+		config.setProperty(Keys.ASSET_FOLDER, "non-existent");
+		URL assetsUrl = this.getClass().getResource("/");
+		File assets = new File(assetsUrl.getFile() + File.separatorChar + "non-existent");
+		Asset asset = new Asset(assets.getParentFile(), readOnlyFolder.getRoot(), config);
+		asset.copy(assets);
 	}
 }
