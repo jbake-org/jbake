0b3120b257ec079ea7ca9204a5050a84a26d8645
aalmiray <aalmiray@gmail.com>
2016-04-27 19:12:55 +0200

    Update extension dependencies

--- src/main/groovy/me/champeau/gradle/JBakeExtension.groovy
+++ src/main/groovy/me/champeau/gradle/JBakeExtension.groovy
@@ -5,11 +5,11 @@ package me.champeau.gradle
  */
 class JBakeExtension {
 
-    String version = '2.3.2'
+    String version = '2.4.0'
     String pegdownVersion = '1.4.2'
     String freemarkerVersion = '2.3.19'
     String asciidoctorJavaIntegrationVersion = '0.1.4'
-    String asciidoctorjVersion = '1.5.1'
+    String asciidoctorjVersion = '1.5.4'
     String srcDirName = 'src/jbake'
     String destDirName = 'jbake'
     boolean clearCache = false
