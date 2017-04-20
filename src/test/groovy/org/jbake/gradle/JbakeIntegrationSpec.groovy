4e9018b7e8328917eebda4a7a1cba8a97b4eff78
aalmiray <aalmiray@gmail.com>
2017-04-20 21:42:43 +0200

    Update to Gradle 3.5

--- src/test/groovy/org/jbake/gradle/JbakeIntegrationSpec.groovy
+++ src/test/groovy/org/jbake/gradle/JbakeIntegrationSpec.groovy
@@ -26,7 +26,7 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 class JbakeIntegrationSpec extends PluginIntegrationSpec {
 
     @Shared
-    def latestGradleVersion = '3.4.1'
+    def latestGradleVersion = '3.5'
 
     @Unroll
     def 'Setup and bake with gradle #version'() {
