ac0a641971d0f6f5dd6ead9b7fe942fd7015d5ec
jaybob007 <4761262+jaybob007@users.noreply.github.com>
2019-10-27 22:22:55 -0400

    Added freemarker outputEncoding property to the config.

--- jbake-core/src/main/java/org/jbake/app/configuration/JBakeProperty.java
+++ jbake-core/src/main/java/org/jbake/app/configuration/JBakeProperty.java
@@ -33,6 +33,7 @@ public class JBakeProperty {
     public static final String RENDER_TAGS = "render.tags";
     public static final String RENDER_TAGS_INDEX = "render.tagsindex";
     public static final String RENDER_ENCODING = "render.encoding";
+    public static final String OUTPUT_ENCODING = "freemarker.outputencoding";
     public static final String SERVER_PORT = "server.port";
     public static final String SITE_HOST = "site.host";
     public static final String SITEMAP_FILE = "sitemap.file";
