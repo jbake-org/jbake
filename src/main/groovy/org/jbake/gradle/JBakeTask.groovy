b9b962109ce10842e23f2c76911f8f1549dc58c7
Daniel Lacasse <daniel@lacasse.io>
2020-06-27 10:20:14 -0400

    Use the worker API for Gradle 5.6 and above
    The worker API prevent memory leaks and fix the concurrency issue.

--- src/main/groovy/org/jbake/gradle/JBakeTask.groovy
+++ src/main/groovy/org/jbake/gradle/JBakeTask.groovy
@@ -26,8 +26,12 @@ import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.util.GradleVersion
+import org.gradle.workers.WorkerExecutor
 import org.jbake.gradle.impl.JBakeProxyImpl
+import org.jbake.gradle.impl.JBakeWorkAction
 
+import javax.inject.Inject
 import java.lang.reflect.Constructor
 
 class JBakeTask extends DefaultTask {
@@ -43,21 +47,38 @@ class JBakeTask extends DefaultTask {
 
     private JBakeProxy jbake
 
-    JBakeTask() {
+    private final WorkerExecutor executor
+
+    @Inject
+    JBakeTask(WorkerExecutor executor) {
         group = 'jbake'
         description = 'Bakes your website with JBake'
+        this.executor = executor
     }
 
     @TaskAction
     void bake() {
-        createJbake()
-        jbake.prepare()
-        mergeConfiguration()
-        jbake.jbake()
-        List<String> errors = jbake.getErrors()
-        if (errors) {
-            errors.each { logger.error(it) }
-            throw new TaskExecutionException(this, new IllegalStateException(errors.join('\n')))
+        // Newer worker API was introduced in 5.6, older worker API doesn't properly forward the logging from Oven which would cause a regression.
+        // See https://github.com/gradle/gradle/issues/8318
+        if (GradleVersion.current().compareTo(GradleVersion.version("5.6")) >= 0) {
+            executor.classLoaderIsolation {
+                it.classpath.from(this.classpath)
+            }.submit(JBakeWorkAction) {
+                it.input = this.input
+                it.output = this.output
+                it.clearCache = this.clearCache
+                it.configuration.putAll(this.configuration)
+            }
+        } else {
+            createJbake()
+            jbake.prepare()
+            mergeConfiguration()
+            jbake.jbake()
+            List<String> errors = jbake.getErrors()
+            if (errors) {
+                errors.each { logger.error(it) }
+                throw new TaskExecutionException(this, new IllegalStateException(errors.join('\n')))
+            }
         }
     }
 
---
+++ src/main/groovy/org/jbake/gradle/impl/JBakeWorkAction.groovy
@@ -0,0 +1,51 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbake.gradle.impl
+
+import org.apache.commons.configuration.CompositeConfiguration
+import org.apache.commons.configuration.MapConfiguration
+import org.gradle.api.logging.Logging
+import org.gradle.workers.WorkAction
+import org.jbake.app.Oven
+
+abstract class JBakeWorkAction implements WorkAction<JBakeWorkActionParameters> {
+    private static final LOGGER = Logging.getLogger(JBakeWorkAction)
+    private Oven jbake
+
+    @Override
+    void execute() {
+        jbake = new Oven(parameters.input.get().asFile, parameters.output.get().asFile, parameters.clearCache.get())
+        jbake.setupPaths()
+        mergeConfiguration()
+        jbake.bake()
+        List<String> errors = jbake.getErrors()
+        if (errors) {
+            errors.each { LOGGER.error(it) }
+            throw new IllegalStateException(errors.join('\n'))
+        }
+    }
+
+    private void mergeConfiguration() {
+        def config = new CompositeConfiguration([createMapConfiguration(), jbake.getConfig()])
+        jbake.setConfig(config)
+    }
+
+    private MapConfiguration createMapConfiguration() {
+        return new MapConfiguration(parameters.configuration.get())
+    }
+}
