497fee3bd5b2ba2b0302d908e7794e2612b5acf7
Jonathan Bullock <jonbullock@gmail.com>
2021-05-12 23:57:49 +0100

    Return empty Map instead of null. Added logging.

--- jbake-core/src/main/java/org/jbake/util/DataFileUtil.java
+++ jbake-core/src/main/java/org/jbake/util/DataFileUtil.java
@@ -2,10 +2,16 @@ package org.jbake.util;
 
 import org.jbake.app.ContentStore;
 import org.jbake.app.DocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class DataFileUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataFileUtil.class);
+
     private ContentStore db;
     private String defaultDocType;
 
@@ -15,10 +21,17 @@ public class DataFileUtil {
     }
 
     public Map<String, Object> get(String ref) {
+        Map<String, Object> result = new HashMap<>();
         DocumentList docs = db.getDocumentByUri(defaultDocType, ref);
-        if (docs.size() == 1) {
-            return docs.get(0);
+        if (docs.isEmpty()) {
+            LOGGER.warn("Unable to locate content for ref: {}", ref);
+        } else {
+            if (docs.size() == 1) {
+                result = docs.get(0);
+            } else {
+                LOGGER.warn("Located multiple hits for ref: {}", ref);
+            }
         }
-        return null;
+        return result;
     }
 }
