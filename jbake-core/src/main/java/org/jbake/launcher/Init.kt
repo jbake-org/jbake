f15fc5314575c6961a02aad2ac787b97aac212cc
Ondrej Zizka <ondrej@zizka.ch>
2025-11-26 11:06:12 +0100

    Shorten

--- jbake-core/src/main/java/org/jbake/launcher/Init.kt
+++ jbake-core/src/main/java/org/jbake/launcher/Init.kt
@@ -11,18 +11,17 @@ import java.io.FileInputStream
  * Initialises sample folder structure with pre-defined template
  */
 class Init(private val config: JBakeConfiguration) {
-    /**
-     */
+
     @Deprecated("use {@link Init#Init(JBakeConfiguration)} instead")
     constructor(config: CompositeConfiguration) : this(DefaultJBakeConfiguration(config))
 
     /**
-     * Performs checks on output folder before extracting template file
+     * Performs checks on output folder before extracting template file.
      *
-     * @param outputFolder                        Target directory for extracting template file
-     * @param templateLocationFolder  Source location for template file
-     * @param templateType                        Type of the template to be used
-     * @throws Exception                            if required folder structure can't be achieved without content overwriting
+     * @param outputFolder            Target directory for extracting template file.
+     * @param templateLocationFolder  Source location for template file.
+     * @param templateType            Type of the template to be used.
+     * @throws Exception              if required folder structure can't be achieved without content overwriting.
      */
     fun run(outputFolder: File, templateLocationFolder: File, templateType: String) {
         if (!outputFolder.canWrite()) {
@@ -33,36 +32,22 @@ class Init(private val config: JBakeConfiguration) {
         var safe = true
         if (contents != null) {
             for (content in contents) {
-                if (content.isDirectory()) {
-                    if (content.getName().equals(config.templateFolderName, ignoreCase = true)) {
-                        safe = false
-                    }
-                    if (content.getName().equals(config.contentFolderName, ignoreCase = true)) {
-                        safe = false
-                    }
-                    if (content.getName().equals(config.assetFolderName, ignoreCase = true)) {
-                        safe = false
-                    }
-                }
+                if (!content.isDirectory()) continue
+                if (content.getName().equals(config.templateFolderName, ignoreCase = true)) safe = false
+                if (content.getName().equals(config.contentFolderName, ignoreCase = true)) safe = false
+                if (content.getName().equals(config.assetFolderName, ignoreCase = true)) safe = false
             }
         }
 
-        if (!safe) {
-            throw Exception(
-                String.format(
-                    "Output folder '%s' already contains structure!",
-                    outputFolder.absolutePath
-                )
-            )
-        }
-        if (config.getExampleProjectByType(templateType) != null) {
-            val templateFile = File(templateLocationFolder, config.getExampleProjectByType(templateType))
-            if (!templateFile.exists()) {
-                throw Exception("Cannot find example project file: " + templateFile.path)
-            }
-            ZipUtil.extract(FileInputStream(templateFile), outputFolder)
-        } else {
+        if (!safe) throw Exception(String.format("Output folder '%s' already contains structure!", outputFolder.absolutePath))
+
+        if (config.getExampleProjectByType(templateType) == null)
             throw Exception("Cannot locate example project type: $templateType")
-        }
+
+        val templateFile = File(templateLocationFolder, config.getExampleProjectByType(templateType))
+        if (!templateFile.exists())
+            throw Exception("Cannot find example project file: " + templateFile.path)
+
+        ZipUtil.extract(FileInputStream(templateFile), outputFolder)
     }
 }
