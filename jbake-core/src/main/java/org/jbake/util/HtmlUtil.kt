b77d8e4ca17ec7ff2a4b54a2f40a6e0de14e4ea8
Ondrej Zizka <ondrej@zizka.ch>
2025-11-26 15:00:40 +0100

    More appropriate name of params, less @param clutter.

--- jbake-core/src/main/java/org/jbake/util/HtmlUtil.kt
+++ jbake-core/src/main/java/org/jbake/util/HtmlUtil.kt
@@ -5,26 +5,19 @@ import org.jbake.model.DocumentModel
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
-/**
- * @author Manik Magar
- */
 object HtmlUtil {
+
     /**
-     * Image paths are specified as w.r.t. assets folder. This function prefix site host to all img src except
-     * the ones that starts with http://, https://.
-     *
+     * Image paths are specified as w.r.t. assets folder. This function prefix site host to all img src except the ones that starts with http://, https://.
      *
      * If image path starts with "./", i.e. relative to the source file, then it first replace that with output file directory and the add site host.
-     *
-     * @param fileContents  Map representing file contents
-     * @param configuration Configuration object
      */
     @JvmStatic
-    fun fixImageSourceUrls(fileContents: DocumentModel, configuration: JBakeConfiguration) {
-        val htmlContent = fileContents.body
+    fun fixImageSourceUrls(documentModel: DocumentModel, configuration: JBakeConfiguration) {
+        val htmlContent = documentModel.body
         val prependSiteHost = configuration.imgPathPrependHost
         val siteHost: String = configuration.siteHost ?: error("siteHost must not be null")
-        val uri = getDocumentUri(fileContents)
+        val uri = getDocumentUri(documentModel)
 
         val document = Jsoup.parseBodyFragment(htmlContent)
         val allImgs = document.getElementsByTag("img")
@@ -34,11 +27,11 @@ object HtmlUtil {
         }
 
         //Use body().html() to prevent adding <body></body> from parsed fragment.
-        fileContents.body = (document.body().html())
+        documentModel.body = (document.body().html())
     }
 
-    private fun getDocumentUri(fileContents: DocumentModel): String {
-        var uri = fileContents.noExtensionUri?.let { removeTrailingSlash(it) } ?: fileContents.uri
+    private fun getDocumentUri(documentModel: DocumentModel): String {
+        var uri = documentModel.noExtensionUri?.let { removeTrailingSlash(it) } ?: documentModel.uri
 
         if (uri.contains("/"))
             uri = removeFilename(uri)
@@ -49,11 +42,11 @@ object HtmlUtil {
         var source = img.attr("src")
 
         // Early return for absolute URLs
-        if (source.startsWith("http://") || source.startsWith("https://")) return
+        if (source.startsWith("http://") || source.startsWith("https://"))
+            return
 
-        if (isRelative(source)) {
+        if (isRelative(source))
             source = uri + source.replaceFirst("\\./".toRegex(), "")
-        }
 
         if (prependSiteHost) {
             val prefix = if (!siteHost.endsWith("/") && isRelative(source)) "$siteHost/" else siteHost
