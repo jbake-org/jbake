fdadd5f621778851562f0e54fdefdfce25bc3c02
Ondrej Zizka <ondrej@zizka.ch>
2025-12-03 12:00:29 +0100

    Shorten DataFileUtil.kt

--- jbake-core/src/main/java/org/jbake/util/DataFileUtil.kt
+++ jbake-core/src/main/java/org/jbake/util/DataFileUtil.kt
@@ -11,19 +11,17 @@ class DataFileUtil(private val db: ContentStore, private val defaultDocType: Str
 
         var result: MutableMap<String,  Any> = HashMap()
         val docs: DocumentList<*> = db.getDocumentByUri(ref)
-        if (docs.isEmpty()) {
-            log.warn("Unable to locate content for ref: {}", ref)
-        }
+
+        if (docs.isEmpty())
+            log.warn("Unable to locate content for ref: $ref")
+        else if (docs.size != 1)
+            log.warn("Located multiple hits for ref: $ref")
         else {
-            if (docs.size != 1)
-                log.warn("Located multiple hits for ref: {}", ref)
-            else {
-                val doc = docs[0]
-                result = if (doc is MutableMap<*, *>) {
-                    @Suppress("UNCHECKED_CAST")
-                    doc as? MutableMap<String, Any> ?: mutableMapOf()
-                } else mutableMapOf()
-            }
+            val doc = docs[0]
+            result = if (doc is MutableMap<*, *>) {
+                @Suppress("UNCHECKED_CAST")
+                doc as? MutableMap<String, Any> ?: mutableMapOf()
+            } else mutableMapOf()
         }
         return result
     }
